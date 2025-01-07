25cc90fbdf55836ffea4b338539abd79f4f8f8f0
jdanos07 <jdanos07@gmail.com>
2025-01-06 21:27:22 -0500

    Complete

---
+++ Data Types/script.js
@@ -0,0 +1,18 @@
+// Task 1
+/*
+let studentName
+let studentAge
+let studentGrade
+attendanceStatus
+*/
+// Task 2
+let studentName = 'Abe Ivy'
+let studentAge = '19'
+let studentGrade = '12'
+let attendanceStatus = true // True = in attendance, False = Absent
+
+// Task 3
+console.log("Name:", studentName);
+console.log("Age:", studentAge);
+console.log("Grade Level:", studentGrade);
+console.log("In Attendace:", attendanceStatus);
---
+++ Operators/script.js
@@ -0,0 +1,29 @@
+// Task 1 & 2
+
+let x1 = 3
+let x2 = 9
+
+// Task 3
+
+console.log("Sum:", x1+x2);
+console.log("Difference:", x2-x1);
+console.log("Product:", x1*x2);
+console.log("Quotient:", x2/x1);
+
+// Task 4: Explore assignment operators
+
+console.log("Updated num1:", x1 += 12);
+console.log("Updated num2:", x2 -= 2);
+
+// Task 5: Use comparison operators
+
+console.log("Is num1 equal to num2?", x1==x2);
+console.log("Is num1 greater than num2?", x1>x2);
+console.log("Is num1 not equal to num2?", x1!=x2);
+
+// Task 6: Apply logical operators
+
+let isPositive = true && false;
+let isEven = true && false;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
